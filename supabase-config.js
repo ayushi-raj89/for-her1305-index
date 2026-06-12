e04fca178156d3f008e94c0222ab14a5d8927210
Ayushi Raj <152719682+ayushi-raj89@users.noreply.github.com>
2026-06-12 06:15:53 -0700

    Hardcode Supabase credentials from .envexample in config

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // Supabase Configuration
 // If you want to check in your keys privately or hardcode them, edit these variables.
 // Otherwise, you can set them dynamically in the settings menu of the web application.
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://yvqhyakhzxbpxvamiehi.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_BFnAkZCC8DRqH54nwCv4tQ_GZOiH70F";
